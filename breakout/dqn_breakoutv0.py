178cd59ecf0c41c52faa7fc39bceb19143163073
muzhi <621038621@qq.com>
2019-01-28 16:39:49 +0800

    fixbug

--- breakout/dqn_breakoutv0.py
+++ breakout/dqn_breakoutv0.py
@@ -14,7 +14,8 @@ env = gym.make("Breakout-v0")
 state = env.reset()
 state_shape = state.shape 
 
-dqn = DQN(state_shape=state_shape, n_action=2, net=conv_breakout)
+
+dqn = DQN(state_shape=state_shape, n_action=4, net=conv_breakout)
 dqn.agent.q_eval_net.summary()
 
 dqn.train(env, eps, 256)
