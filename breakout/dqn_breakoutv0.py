b10d617d78ae9fef71631ed8c9a5827ab283ab1f
muzhi <621038621@qq.com>
2019-01-28 18:28:37 +0800

    add gpu

--- breakout/dqn_breakoutv0.py
+++ breakout/dqn_breakoutv0.py
@@ -12,8 +12,11 @@ eps = int(sys.argv[1])
 
 env = gym.make("Breakout-v0")
 state = env.reset()
-state_shape = state.shape 
+state_shape = state.shape
 
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+gpu_options = tf.GPUOptions(allow_growth=True)
+sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
 
 dqn = DQN(state_shape=state_shape, n_action=4, net=conv_breakout)
 dqn.agent.q_eval_net.summary()
